0f760110f51fc8948d54516f61385506c1133a54
Mewlips <mewlips@gmail.com>
2013-12-26 14:28:13 +0900

    get_buffer, release_buffer

--- src/mediaplayer/ffmpeg_decoder.rs
+++ src/mediaplayer/ffmpeg_decoder.rs
@@ -2,8 +2,14 @@ use avcodec;
 use av_stream::AVStream;
 use avutil;
 use std::ptr::{mut_null,to_mut_unsafe_ptr};
-use std::cast::{transmute_immut_unsafe};
+use std::cast::{transmute,transmute_immut_unsafe};
 use util;
+use std::libc::c_int;
+use std::mem::size_of;
+
+pub struct DecoderUserData {
+    pts: u64,
+}
 
 pub struct FFmpegDecoder {
     codec_ctx: *mut avcodec::AVCodecContext,
@@ -41,6 +47,10 @@ impl FFmpegDecoder {
             error!("AVError: {}", util::av_strerror(result));
             return None;
         }
+        unsafe {
+            (*codec_ctx).opaque = avutil::av_malloc(size_of::<DecoderUserData>() as u64);
+        }
+
         debug!("VideoDecoder::new() end");
         Some(FFmpegDecoder {
             codec_ctx: codec_ctx,
@@ -48,5 +58,16 @@ impl FFmpegDecoder {
             decoder: decoder,
             options: options,
         })
+    }
+}
+
+impl Drop for FFmpegDecoder {
+    fn drop(&mut self) {
+        println("FFmpegDecoder::drop()");
+        unsafe {
+            if !(*self.codec_ctx).opaque.is_null() {
+                avutil::av_freep(transmute(&mut (*self.codec_ctx).opaque));
+            }
+        }
     }
 }
--- src/mediaplayer/video_decoder.rs
+++ src/mediaplayer/video_decoder.rs
@@ -1,10 +1,11 @@
 use avcodec;
 use av_stream::AVStream;
 use avutil;
-use std::cast::{transmute_immut_unsafe};
+use std::cast::{transmute,transmute_immut_unsafe};
 use std::libc::{c_int};
 use std::ptr::{to_mut_unsafe_ptr};
-use ffmpeg_decoder::FFmpegDecoder;
+use ffmpeg_decoder::{DecoderUserData,FFmpegDecoder};
+use std::mem::size_of;
 
 pub struct VideoDecoder {
     decoder: FFmpegDecoder,
@@ -23,6 +24,10 @@ impl VideoDecoder {
                 let height = codec_ctx.height as int;
                 let pix_fmt = codec_ctx.pix_fmt;
                 let time_base = video_stream.get_time_base();
+                unsafe {
+                    (*decoder.codec_ctx).get_buffer = get_buffer;
+                    (*decoder.codec_ctx).release_buffer = release_buffer;
+                }
                 Some(VideoDecoder {
                     decoder: decoder,
                     width: width,
@@ -56,11 +61,23 @@ impl VideoDecoder {
                     let mut got_frame: c_int = 0;
                     let frame = avcodec::avcodec_alloc_frame();
                     let mut pts = 0f64;
+                    let user_data: *mut DecoderUserData = transmute((*codec_ctx).opaque);
+                    (*user_data).pts = (*packet).pts as u64;
                     avcodec::avcodec_decode_video2(codec_ctx, frame,
                                                    to_mut_unsafe_ptr(&mut got_frame),
                                                    transmute_immut_unsafe(packet));
-                    if (*packet).dts != avutil::AV_NOPTS_VALUE {
+                    if (*packet).dts == avutil::AV_NOPTS_VALUE &&
+                       !(*frame).opaque.is_null() {
+                        let frame_pts: *u64 = transmute((*frame).opaque);
+                        if *frame_pts == avutil::AV_NOPTS_VALUE as u64 {
+                            pts = (*packet).dts as f64;
+                        }
+                        println!("use pts");
+                    } else if (*packet).dts != avutil::AV_NOPTS_VALUE {
+                        println!("use dts");
                         pts = (*packet).dts as f64;
+                    } else {
+                        pts = 0f64;
                     }
                     pts = ((pts as f64) * avutil::av_q2d(time_base)) as f64;
                     println!("pts = {}", pts);
@@ -80,4 +97,27 @@ impl VideoDecoder {
             }
         }
     }
+}
+
+extern fn get_buffer(codec_ctx: *mut avcodec::AVCodecContext,
+                     pic: *mut avcodec::AVFrame) -> c_int {
+    let ret = unsafe {
+        avcodec::avcodec_default_get_buffer(codec_ctx, pic)
+    };
+    unsafe {
+        let user_data: *mut DecoderUserData = transmute((*codec_ctx).opaque);
+        let pts: *mut u64 = transmute(avutil::av_malloc(size_of::<u64>() as u64));
+        (*pts) = (*user_data).pts;
+        (*pic).opaque = transmute(pts);
+        println!("get_buffer(): pts = {}", (*pts));
+    }
+    ret
+}
+
+extern fn release_buffer(codec_ctx: *mut avcodec::AVCodecContext,
+                         pic: *mut avcodec::AVFrame) {
+    unsafe {
+        avcodec::avcodec_default_release_buffer(codec_ctx, pic);
+    }
+    println!("release_buffer()");
 }
